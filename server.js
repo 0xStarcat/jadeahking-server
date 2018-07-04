770d2ba5e7774fc81b2ec22f633f58e75c410f75
Jade <jeffahking@gmail.com>
2018-07-04 14:39:06 -0400

    update new proejct

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-
 // server/app.js
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const subdomain = require('express-subdomain');
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const subdomain = require('express-subdomain')
 
 const app = express()
-  // .use(subdomain('blog', express.static('/var/www/blog/_site/index.html')));
+// .use(subdomain('blog', express.static('/var/www/blog/_site/index.html')));
 
 var port = 8080
 // Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+  )
+)
 
 // Serve multiple static assets
 // http://stackoverflow.com/questions/5924072/express-js-cant-get-my-static-files-why
-app.use(express.static(path.resolve(__dirname, '../project-fluffypurrkins/src/Client/Build')));
-app.use('/stuff/public', express.static(path.resolve(__dirname, '../some_forms/public')));
+app.use(express.static(path.resolve(__dirname, '../project-fluffypurrkins/src/Client/Build')))
+app.use('/top-secret/public', express.static(path.resolve(__dirname, '../some_forms/public')))
 // Always return the main index.html, so react-router render the route in the client
 app.get(['/', '/about', '/cv', '/contact'], (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../project-fluffypurrkins/src/Client/Build', 'index.html'));
-});
+  res.sendFile(path.resolve(__dirname, '../project-fluffypurrkins/src/Client/Build', 'index.html'))
+})
 
-app.get('/stuff/:project', (req, res) => {
+app.get('/top-secret/:project', (req, res) => {
   switch (req.params.project) {
-    case 'r2r':
-      res.sendFile(path.resolve(__dirname, '../some_forms', 'index.html'));
-      break
-    case 'r2r-b':
-      res.sendFile(path.resolve(__dirname, '../some_forms', 'index-B.html'));
+    case 'nyc-data':
+      res.sendFile('/var/www/nyc_data/build/index.html')
       break
     default:
-      res.sendFile(path.resolve(__dirname, './', 'NotFound.html'));
+      res.sendFile(path.resolve(__dirname, './', 'NotFound.html'))
       break
   }
-});
+})
 
-app.get('*', function(req, res){
-  res.sendFile(path.resolve(__dirname, './', 'NotFound.html'));
-});
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, './', 'NotFound.html'))
+})
 
-app.listen(port, function()
-{
-  console.log('Server running on port: '+port);
+app.listen(port, function() {
+  console.log('Server running on port: ' + port)
 })
