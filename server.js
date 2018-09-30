ee53ef9f964c4ffd0b7d4cba1eab2b47bd3c064e
angel <xxStarcat@gmail.com>
2018-09-29 23:00:12 -0400

    edit path

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/slides/freelancing/cards', (req, res) => {
 })
 
 app.get('/slides/freelancing/cards/:card', (req, res) => {
-  res.sendFile('/var/www/freelance_presentation/' + req.params["card"].split('.html')[0] + '.html')
+  res.sendFile('/var/www/freelance_presentation/' + req.params["card"]
 })
 
 
